da46e3ce085b0bcd27adca055d97f419800eaeb8
Yagnesh-Vaja <yvtechcoder.com>
2025-02-10 13:02:44 +0530

    Initial commit

---
+++ 01.Basics/app.js
@@ -0,0 +1,49 @@
+// alert("Hello my nameis yagnesh");
+// console.log("Hello Yagnesh");
+// console.warn("This is warning");
+// console.error("This is error");
+// console.table({ name: "yagnesh", age: 22 });
+// console.log("Hello...");
+// console.log("Hello...");
+// console.log("Hello...");
+// console.log("Hello...");
+// console.log("Hello...");
+// console.clear();
+
+// console.log("Hello...");
+// console.log("Hello...");
+
+//Comments
+// clg -> console.log
+// log -> console.log
+
+//Variables
+//(reserved keyword) (variable name) (type of value)
+
+//Declare  variable
+// let name;
+
+//Assign value
+// let age;
+// age = 22;
+// console.log(age);
+
+// let yagnesh = "Full stack developer";
+// console.log(yagnesh);
+// yagnesh = "variable";
+// console.log(yagnesh);
+
+// Challenge-1
+// let name = "Yagnesh";
+// let whatDoYouWannaBecomeInYourLife = "Programmer";
+// let gender = "Male";
+// let twitterHandle = "Twitter";
+
+// console.log(name);
+// console.log(whatDoYouWannaBecomeInYourLife);
+// console.log(gender);
+// console.log(twitterHandle);
+
+//Primitive Types
+// 1.Number
+let;
