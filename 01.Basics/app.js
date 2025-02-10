5a2eece508e0d9bcdfab8eccc03b54da6000dbbc
Yagnesh-Vaja <yvtechcoder.com>
2025-02-10 16:25:03 +0530

    Initial commit

--- 01.Basics/app.js
+++ 01.Basics/app.js
@@ -46,4 +46,52 @@
 
 //Primitive Types
 // 1.Number
-let;
+// let num = 10;
+// num = 10.2;
+// console.log(num);
+// console.log(typeof num);
+
+// let num = 2;
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+// console.log(2 ** 2);
+
+// let counter = 10;
+// counter++;
+// counter++;
+// counter++;
+// counter++;
+// counter++;
+// counter--;
+// counter--;
+
+// console.log(counter);
+
+//Challenge-2
+
+// let firstFavNum = 27;
+// console.log(firstFavNum);
+
+// let secondFavNum = 70;
+// console.log(secondFavNum);
+
+// let sum = firstFavNum + secondFavNum;
+// console.log(sum);
+
+// let sub = firstFavNum - secondFavNum;
+// console.log(sub);
+
+// let mul = firstFavNum * secondFavNum;
+// console.log(mul);
+
+// let div = firstFavNum / secondFavNum;
+// console.log(div);
+
+// let mod = firstFavNum % secondFavNum;
+// console.log(mod);
+
+// let power = firstFavNum ** secondFavNum;
+// console.log(power);
