f0e18e1d56789b5006287a3c09a533201bf6734c
Yagnesh-Vaja <yvtechcoder.com>
2025-02-12 17:37:10 +0530

    Initial commit

--- 01.Basics/app.js
+++ 01.Basics/app.js
@@ -95,3 +95,175 @@
 
 // let power = firstFavNum ** secondFavNum;
 // console.log(power);
+
+// Boolean -> true||false
+// let isTrue = true;
+// console.log(isTrue);
+
+// let isFalse = false;
+// console.log(isFalse);
+
+// let isNan = NaN;
+// console.log(isNan);
+
+// let number = 10;
+// console.log(number + undefined);
+
+//Falsy
+// false,null,undefined,0,-0,NaN,'',"",``,(empty quotes)
+// Other than this all are truthy values
+
+// let notDefined = undefined; //Set by JS Compiler
+// console.log(notDefined); //
+
+// let notDefined = null; //Set by Programmer
+// console.log(notDefined);
+
+//Challenge-3
+// let isJsProgrammingLanguage = true;
+// console.log(isJsProgrammingLanguage);
+
+// let isJsHard = false;
+// console.log(isJsHard);
+
+// let favNumb = 27;
+// console.log(favNumb + undefined);
+
+// let favNumber = undefined;
+// console.log(favNumber);
+
+//Comparision operator
+//->Relational operator
+//>Greater then
+//<Less then
+//>=Greater then or equal to
+//<=Less then or equal to
+//->Equality operator
+
+//->Relational operator
+// console.log(10 + 10);
+// console.log(10 > 10);
+// console.log(10 < 10);
+// console.log(10 >= 10);
+// console.log(10 <= 10);
+
+//->Equality operator
+// === strict equality (type+value)
+// !== strict non-equality operator (type+value)
+// == lose equality operator (value)
+// != not equality operator (value)
+
+// console.log(10 === 10);
+// console.log(10 === "10");
+// console.log(10 === 20);
+// console.log(10 !== "10");
+// console.log(10 == 10);
+// console.log(10 != 10);
+
+//Challenge-4
+
+// let firstFavNumb = 10;
+// let secondFavNumb = 20;
+// console.log(firstFavNumb > secondFavNumb);
+// console.log(firstFavNumb < secondFavNumb);
+// console.log(firstFavNumb >= secondFavNumb);
+// console.log(firstFavNumb <= secondFavNumb);
+// console.log(firstFavNumb === secondFavNumb);
+// console.log(firstFavNumb == secondFavNumb);
+// console.log(firstFavNumb !== secondFavNumb);
+// console.log(firstFavNumb != secondFavNumb);
+
+//Strings
+// let firstName = "yagnesh";
+// let middleName = "Dinesh";
+// let surName = `Vaja`;
+// console.log(firstName);
+// console.log(middleName);
+// console.log(surName);
+// let fullName = firstName + " " + middleName + " " + surName;
+// console.log(fullName);
+
+//1. Concatenation
+// let firstName = "yagnesh";
+// let middleName = "Dinesh";
+// let fullName = firstName.concat(middleName);
+// console.log(fullName);
+
+//2.Append
+// firstName += "Programmer";
+// console.log(firstName);
+
+//3.length
+// console.log(firstName.length);
+
+//4.Cases
+// console.log(firstName.toLowerCase());
+// console.log(firstName.toUpperCase());
+
+//5.Slice
+// console.log(firstName.slice(0, 3));
+
+//6.Split and join
+// console.log(firstName.split("").join("-"));
+
+//7.Includes
+// console.log(firstName.includes("y"));
+
+//8.Trim
+// console.log(firstName.trim());
+
+// let desc = "This is random text";
+// console.log(desc);
+
+// let fName = "Yagnesh";
+// let lName = "Vaja";
+// let fullName = `${fName} ${lName}`;
+// console.log(fullName);
+
+// Challenge-5
+
+// let favActorFirstName = "Salman";
+// let favActorLastName = "Khan";
+// let fullName = `${favActorFirstName} ${favActorLastName}`;
+// let toUpperCase = favActorFirstName.toUpperCase();
+// let message = `My favorite actor is ${favActorFirstName.toUpperCase()} and is a real hero in film industry `;
+// message += "his best show is silicon valley";
+// console.log(favActorFirstName);
+// console.log(favActorLastName);
+// console.log(fullName);
+// console.log(toUpperCase);
+// console.log(message);
+
+//Type Conversion
+// let money = "50";
+// console.log(typeof money);
+
+//Convert string to number
+// money = parseInt(money);
+// console.log(typeof money);
+// let money = "50";
+// console.log(money);
+// console.log(typeof money);
+// money = Number(money);
+// console.log(money);
+// console.log(typeof money);
+// money = +money;
+// console.log(money);
+// console.log(typeof money);
+
+//Convert number to string
+// let money = 50;
+// console.log(money);
+// console.log(typeof money);
+// // money = money.toString();
+// // console.log(typeof money);
+// money = String(money);
+// console.log(typeof money);
+
+//Type Conversion
+//Convert string to decimal
+// let money = "50.245";
+// console.log(money);
+// console.log(typeof money);
+// money = parseFloat(money);
+// console.log(typeof money);
---
+++ 02.Control Flow/app.js
@@ -0,0 +1,158 @@
+// let a = 20;
+// let b = 30;
+// if (a > b) {
+//   console.log("a is greater than b");
+// } else if (a < b) {
+//   console.log("a is less than b");
+// } else {
+//   console.log("a is equal to b");
+// }
+
+// let time = 10;
+// let greetings;
+
+//Conditional statements
+// if (time < 10) {
+//   greetings = "Good Morning";
+//   console.log(greetings);
+// } else if (time < 20) {
+//   greetings = "Good Day";
+//   console.log(greetings);
+// } else {
+//   greetings = "Good Evening";
+//   console.log(greetings);
+// }
+
+// Challenge-1
+
+// let password = 7;
+// if (password === 8) {
+//   console.log("Welcome");
+// } else if (password <= 8) {
+//   console.log("Password is too short");
+// } else if (password >= 8) {
+//   console.log("Too Long Password and password should be 8 characters");
+// } else {
+//   console.log("Please provide a password");
+// }
+
+//Switch Statements
+
+// let x = 11;
+// let bulb;
+
+// switch (x) {
+//   case 0:
+//     bulb = "Off";
+//     console.log(bulb);
+//     break;
+//   case 1:
+//     bulb = "On";
+//     console.log(bulb);
+//     break;
+//   default:
+//     bulb = "No value found";
+//     console.log(bulb);
+// }
+
+// let day = "Tuesday";
+
+// switch (day) {
+//   case "Monday":
+//     console.log("Today is Monday");
+//     break;
+
+//   case "Tuesday":
+//     console.log("Today is Tuesday");
+//     break;
+
+//   case "Wednesday":
+//     console.log("Today is Wednesday");
+//     break;
+
+//   case "Thrusday":
+//     console.log("Today is Thrusday");
+//     break;
+
+//   case "Friday":
+//     console.log("Today is Friday");
+//     break;
+
+//   case "Saturday":
+//     console.log("Today is Saturday");
+//     break;
+
+//   case "Sunday":
+//     console.log("Today is Sunday");
+//     break;
+
+//   default:
+//     console.log("Invalid Day");
+// }
+
+// Challenge -2
+// let fruits = "Banana";
+
+// switch (fruits) {
+//   case "Banana":
+//     console.log("Banana is good");
+//     break;
+//   case "Orange":
+//     console.log("I am not a fan of orange");
+//     break;
+//   case "Apple":
+//     console.log("How you like them apples?");
+//     break;
+//   default:
+//     console.log("I have never heard of that fruit");
+// }
+
+//For loop
+// DRY ->Don't Repeat Yourself
+// for (let i = 1; i <= 5; i++) {
+//   console.log("Hello World", i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log("--- Outer Loop ---");
+//   for (let j = 1; j <= 5; j++) {
+//     console.log("--- Inner Loop ---");
+//   }
+// }
+
+// for (let i = 0; i <= 1000; i++) {
+//   console.log("Yagnesh ", i);
+// }
+
+//While Loop
+// let i = 1;
+// while (i <= 5) {
+//   console.log("Hello World", i);
+//   i++;
+// }
+
+// let i = 10;
+// while (i <= 100) {
+//   console.log("Yagnesh Vaja", i);
+//   i++;
+// }
+
+//Do While loop
+
+// let i = 1;
+// do {
+//   console.log("Yagnesh Vaja", i);
+//   i++;
+// } while (i <= 5);
+
+// let i = 20;
+// do {
+//   console.log("Yagnesh Vaja", i);
+//   i++;
+// } while (i <= 400);
+
+//1. ->Logical And(&&)
+//T-T=T else all are false
+//2. ->Logical OR(||)
+//F-F=F else all are true`
+//3. ->Logical NOT(!)
